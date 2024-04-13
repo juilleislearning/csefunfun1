6bd8251d4db4e24a9bd816dc93cef6d889ccbbda
Mai Chi <maichi@juilles-Mac.local>
2024-04-13 16:12:57 +0900

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <h1>Hello Professor! Thank you for such inspiring lectures!</h1>
+      <h2>(Pleaseee go easy on us with mid-term... The cat is basically me at this point...)</h2>
+    </Layout>
+  );
+}
